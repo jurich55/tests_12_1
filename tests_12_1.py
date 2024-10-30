75dccd53110526c340aec5cb98fcc1ac69531336
Vladimir <sv_urich@mail.ru>
2024-10-30 13:39:29 +0400

    Update tests_12_1.py

--- tests_12_1.py
+++ tests_12_1.py
@@ -1,5 +1,18 @@
-import tests_12_1_run
 import unittest
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
 
 class RunnerTest(unittest.TestCase):
     def test_run(self):
