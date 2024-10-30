a0e3db04304d0dad15f1ad1ae8ce473f881bb494
Vladimir <sv_urich@mail.ru>
2024-10-30 13:14:03 +0400

    Add files via upload

---
+++ tests_12_1.py
@@ -0,0 +1,24 @@
+import tests_12_1_run
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_run(self):
+        for run in range(10):
+            return range
+        self.assertEqual(self.distance, 100)
+
+    def test_walk(self):
+        for walk in range(10):
+            return range
+        self.assertEqual(self.distance, 50)
+
+    def test_challenge(self):
+        for walk in range(10):
+            return walk
+        for run in range(10):
+            return  run
+        self.assertNotEqual(distance)
+
+if __name__ == '__main__':
+    unittest.main()
+
